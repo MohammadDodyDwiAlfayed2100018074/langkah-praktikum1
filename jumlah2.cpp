168f78974d96d61dd6ebb2ada068aa8ac3ce8969
MohammadDodyDwiAlfayed2100018074 <101861108+MohammadDodyDwiAlfayed2100018074@users.noreply.github.com>
2022-03-24 16:38:32 +0700

    Add files via upload

---
+++ jumlah2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ #include <conio.h>
+ #include "Dody_hitung2.h"
+
+using namespace std;
+ main() {
+ Hitung X;
+ cin >> X;
+ X.hitung_jumlahnya();
+  cout << X;
+ getch();
+ return 0;
+ }
+
